9fa6914c3b30b556786677a3a25de9751f28a254
antilou86 <antilou86@gmail.com>
2019-12-13 14:03:43 -0500

    completed count_th.py

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,12 +7,15 @@ def count_th(word):
     count = 0
     index = 0
     def count_helper(word, index, count):
-        if index >= len(list(word)):
+        if index >= len(list(word))-1:
             return count
         elif list(word)[index] == "t" and list(word)[index+1] =="h":
             count += 1
             index += 2
             return count_helper(word, index, count)
         else: 
-            return count_helper(word, index + 1, count)
+            index += 1
+            return count_helper(word, index, count)
     return count_helper(word, index, count)
+
+
