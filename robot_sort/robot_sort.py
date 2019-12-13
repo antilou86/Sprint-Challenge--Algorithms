58aad6366d5ecc7ddddf001d76489cbf3768b134
antilou86 <antilou86@gmail.com>
2019-12-13 17:15:21 -0500

    wow. three total re-writes of the robot problem. but MVP COMPLETE.

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,31 +97,46 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        for i in l: 
-            set_light_off()
+        while True:
 
-            for num in range(0 - len(l) - 1):
+            #initial swap
+            self.swap_item()
+            self.move_right()
 
-                if can_move_left == False:
-                    swap_item
-                    move_right
-                elif can_move_left and can_move_right and compare_item == 1:
-                    swap_item
-                    move_right
-                elif can_move_left and can_move_right and compare_item == -1:
-                    move_right
-                elif can_move_left == True and can_move_right != True and compare_item == 1:
-                    swap_item
-                    set_light_on()
-        pass
+            if self.compare_item() == 1:
 
+                #if swapped needed scootch n swap
+                self.swap_item()
+                self.move_left()
+                self.swap_item()
+                self.move_right()
+                #turn on light if we've made a swap
+                self.set_light_on()
+            else:
+                #if swap was not needed, undo initial swap
+                self.move_left()
+                self.swap_item()
+                self.move_right()
 
+            if not self.can_move_right():
+                #if we've hit the end
+
+                if not self.light_is_on():
+                    # if its not bright, we did it right and finish.
+                    break
+                else:
+                    #go back to beginning
+                    while self.can_move_left():
+                        self.move_left()
+
+                    self.set_light_off() #turn the light off and start next loop
+   
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+ 
     robot = SortingRobot(l)
 
     robot.sort()
